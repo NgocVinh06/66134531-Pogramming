9b15a5674c607985b3e7908ea004f4dd22939525
NgocVinh06 <vinh.nn.66cntt@ntu.edu.vn>
2026-02-03 12:42:11 +0700

    Tinhtoan
    ví dụ 3

---
+++ Tinhtoan/src/nv/edu/Tinhtoan.java
@@ -0,0 +1,17 @@
+package nv.edu;
+
+public class Tinhtoan {
+
+	public static void main(String[] args) {
+		//tinh toan don gian
+		int a =4;
+		int b=2;
+		
+		System.out.println("Cộng: "+ (a+b));
+		System.out.println("Trừ: "+ (a-b));
+		System.out.println("Nhân: "+ (a*b));
+		System.out.println("Chia: "+ (a/b));
+
+	}
+
+}
