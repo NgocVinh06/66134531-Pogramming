30ddb46988363856b0490b903012de1545f1a542
NgocVinh06 <vinh.nn.66cntt@ntu.edu.vn>
2026-02-03 12:59:51 +0700

    Kieu du lieu

---
+++ Kieudulieu/src/nv/edu/Kieudulieu.java
@@ -0,0 +1,20 @@
+package nv.edu;
+
+public class Kieudulieu {
+
+	public static void main(String[] args) {
+		String tensach = "Lap trinh java";
+		int namxuatban = 2026;
+		double giatien = 15.5;
+		boolean conSach = true;
+		char makho = 'M';
+		
+		System.out.println("Ten sach: "+ tensach);
+		System.out.println("Nam xuat ban: "+namxuatban);
+		System.out.println("gia tien: "+ giatien);
+		System.out.println("Con sach: "+conSach);
+		System.out.println("ma kho: "+ makho);
+
+	}
+
+}
