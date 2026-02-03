60ac02d5ff7957ce7808a8e6d084669a4ade4044
NgocVinh06 <vinh.nn.66cntt@ntu.edu.vn>
2026-02-03 12:50:49 +0700

    Khaibaobien
    vidu

---
+++ khaiBaoBien/src/nv/edu/vidu.java
@@ -0,0 +1,17 @@
+package nv.edu;
+
+public class vidu {
+
+	public static void main(String[] args) {
+		// khai bao bien
+		String Thongbao = "Xin chao";
+		String Hovaten ="Nguyen Ngoc Vinh";
+		String MSSV = "66134531";
+				
+		System.out.println("Thong bao: "+ Thongbao);
+		System.out.println("Ho va ten: "+ Hovaten);
+		System.out.println("Mssv: "+ MSSV);
+
+	}
+
+}
