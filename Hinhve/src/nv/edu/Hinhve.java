4aa06581b1d9ec02232a787b132197c62a22d9a1
NgocVinh06 <vinh.nn.66cntt@ntu.edu.vn>
2026-02-03 12:37:32 +0700

    vi du 2
    Hinh ve

---
+++ Hinhve/src/nv/edu/Hinhve.java
@@ -0,0 +1,13 @@
+package nv.edu;
+
+public class Hinhve {
+
+	public static void main(String[] args) {
+		System.out.println("    *   ");
+		System.out.println("   ***  ");
+		System.out.println("  ***** ");
+		System.out.println(" ******* ");
+
+	}
+
+}
