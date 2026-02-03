68d1946f9481ed5502f3531b3a923cbb1facc8e3
NgocVinh06 <vinh.nn.66cntt@ntu.edu.vn>
2026-02-03 12:25:49 +0700

    Update Xinchao.java

--- HelloWorld/src/nv/edu/Xinchao.java
+++ HelloWorld/src/nv/edu/Xinchao.java
@@ -5,6 +5,7 @@ public class Xinchao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Xin chao");
+		System.out.println("toi them noi dung");
 
 	}
 
