4a8ee88363a36518a617a72d6998dac805954e0c
NgocVinh06 <vinh.nn.66cntt@ntu.edu.vn>
2026-03-22 14:38:27 +0700

    xong run nv

---
+++ ViduOPP_KeThua_quanlynhanvien/src/nv/quanlynhanvien/Nhanvienvanphong.java
@@ -0,0 +1,8 @@
+package nv.quanlynhanvien;
+
+public class Nhanvienvanphong {
+	private double sogiolamthem;
+	private double luongthemmoigio;
+	// hàm tạo
+	public Nhanvienvanphong(String maNV, String hoTen, double luongCoBan, double sogiolamthem)
+}
--- ViduOPP_KeThua_quanlynhanvien/src/nv/quanlynhanvien/Run.java
+++ ViduOPP_KeThua_quanlynhanvien/src/nv/quanlynhanvien/Run.java
@@ -6,6 +6,10 @@ public class Run {
 		// Test lop nhan vien
 		NhanVien nv1 = new NhanVien("001", "Nguyen Ngoc Vinh", 2300000);
 		nv1.hienthiThongTin();
+		System.out.print(nv1.toString());
+		
+		Nhanvienvanphong nvvanphong1 = new Nhanvienvanphong();
+		System.out.println(nvvanphong1.toString());
 
 	}
 
