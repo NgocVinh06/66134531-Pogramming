ec8de9ceb669e5334bf1933c360e4a1a5d4dcaee
NgocVinh06 <vinh.nn.66cntt@ntu.edu.vn>
2026-03-22 13:36:09 +0700

    viet main, test lop nhan vien

---
+++ ViduOPP_kethua_QLNV/src/nv/qlnv/Run.java
@@ -0,0 +1,12 @@
+package nv.qlnv;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// Test lop nhan vien
+		Nhanvien nv1 = new Nhanvien("001", "Nguyen Ngoc Vinh", 2600000);
+		nv1.hienthithongtin();
+
+	}
+
+}
