fd5005ce782caa95b106cf3ce13d48f7b75d6094
NgocVinh06 <vinh.nn.66cntt@ntu.edu.vn>
2026-03-22 14:20:04 +0700

    viết main test lớp nhân viên

---
+++ ViduOPP_KeThua_quanlynhanvien/src/nv/quanlynhanvien/Run.java
@@ -0,0 +1,12 @@
+package nv.quanlynhanvien;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// Test lop nhan vien
+		NhanVien nv1 = new NhanVien("001", "Nguyen Ngoc Vinh", 2300000);
+		nv1.hienthiThongTin();
+
+	}
+
+}
