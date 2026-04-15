cb09b0d1f44f33c0b09a6fe12e88e3c19896917d
NgocVinh06 <vinh.nn.66cntt@ntu.edu.vn>
2026-04-16 01:43:26 +0700

    vidu trang 73 - 75

---
+++ OPP_BufferedReader_vd2/src/nv/ntu/ReadLine.java
@@ -0,0 +1,18 @@
+package nv.ntu;
+import java.io.*;
+class ReadLine {
+    public static void main(String args[]) throws IOException {
+        // Tạo BufferedReader dùng System.in
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+
+        System.out.println("Nhap chuoi.");
+        System.out.println("Nhap 'stop' ket thuc chuong trinh.");
+
+        do {
+            str = br.readLine();
+            System.out.println(str);
+        } while (!str.equals("stop"));
+    }
+}
